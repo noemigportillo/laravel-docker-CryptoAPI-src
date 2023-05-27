dbda526a373b1861b2dcad77ddb0a678206e7543
Adriana <marin.138631@e.unavarra.es>
2023-05-27 23:26:05 +0000

    modificando SellCoinService

--- app/Infrastructure/Persistence/ApiCoinDataSource/ApiCoinRepository.php
+++ app/Infrastructure/Persistence/ApiCoinDataSource/ApiCoinRepository.php
@@ -3,13 +3,14 @@
 namespace App\Infrastructure\Persistence\ApiCoinDataSource;
 
 use App\Domain\Coin;
+use App\Infrastructure\Persistence\APICliente;
 
 class ApiCoinRepository
 {
     private Coin $coin;
     public function buySell(string $coinId, float $amountUSD): ?Coin
     {
-        $api = new ApiCoinRepository();
+        $api = new APICliente();
         $this->coin = $api->getCoinInfo($coinId);
         $priceCoinUsd = $this->coin->getValueUsd();
         $amountCoin = $amountUSD / $priceCoinUsd;
