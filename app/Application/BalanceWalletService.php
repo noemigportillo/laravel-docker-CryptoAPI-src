de857c0afaf99ef1ff208c1e5b45abce5b5ff91f
David <davidalvarezg05@gmail.com>
2023-06-09 09:30:55 +0000

    [32]-Creacion del service del balance

---
+++ app/Application/BalanceWalletService.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Application;
+
+use App\Application\Exceptions\WalletNotFoundException;
+use App\Domain\Wallet;
+use App\Infrastructure\Persistence\APICliente;
+
+class BalanceWalletService
+{
+    private WalletDataSource $walletDataSource;
+
+    /**
+     * @param WalletDataSource $walletDataSource
+     */
+    public function __construct(WalletDataSource $walletDataSource)
+    {
+        $this->walletDataSource = $walletDataSource;
+    }
+
+
+    public function execute(string $wallet_id): ?array
+    {
+        $api = new APICliente();
+        $wallet = $this->walletDataSource->getWalletInfo($wallet_id);
+        if (is_null($wallet)) {
+            throw new WalletNotFoundException();
+        }
+        $balance = $wallet->getBalanceUsd();
+        $coinsWallet = $wallet->getCoins();
+        foreach ($coinsWallet as $coin) {
+            if (is_null($api->getCoinInfo($coin->getId()))) {
+                return null;
+            }
+            $newValuesCoin = $api->getCoinInfo($coin->getId());
+            $balance = $balance + ($newValuesCoin->getValueUsd() * $coin->getAmount());
+        }
+        return $balance;
+    }
+}
